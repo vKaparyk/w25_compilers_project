bcb9ef6650209d9e10a19c68002fbffbbfe76177
BenpaiXD <b.bliss@rogers.com>
2025-02-25 19:11:20 +0000

    oops missed some

---
+++ absyn/CompoundExp.java
@@ -0,0 +1,17 @@
+package absyn;
+
+public class CompoundExp extends Exp {
+    public VarDecList decs;
+    public ExpList exps;
+    public ExpList elsepart;
+
+    public CompoundExp(int pos, VarDecList decs, ExpList exps) {
+        this.pos = pos;
+        this.decs = decs;
+        this.exps = exps;
+    }
+
+    public void accept(AbsynVisitor visitor, int level) {
+        visitor.visit(this, level);
+    }
+}
