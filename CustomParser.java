3c5d38984781eb8a5d8cf2e9828959fc08f356e8
ukaparyk <ukparayk@uoguelph.ca>
2025-03-02 23:55:27 -0500

    fix nonassoc and printing

--- CustomParser.java
+++ CustomParser.java
@@ -4,17 +4,29 @@ public class CustomParser extends parser {
     private String fileName;
 
     public CustomParser(Lexer lexer, String fileName) {
-        super(lexer); 
+        super(lexer);
         this.fileName = fileName;
     }
 
     public void syntax_error(Symbol info) {
-        System.err.print(this.fileName + ":"
-                + (info.left + 1) + ":"
-                + (info.right + 1) + ": ");
+        // System.err.print(this.fileName + ":"
+        // + (info.left + 1) + ":"
+        // + (info.right + 1) + ": ");
+        return;
     }
 
     public void report_error(String message, Object info) {
+        // // System.err.println(message);
+        // // }
+
+        // public void report_error(String message, Symbol info) {
+        // System.err.println(info);
+        if (info instanceof Symbol) {
+            Symbol info2 = (Symbol) info;
+            System.err.print(this.fileName + ":"
+                    + (info2.left + 1) + ":"
+                    + (info2.right + 1) + ": ");
+        }
         System.err.println(message);
     }
 }
